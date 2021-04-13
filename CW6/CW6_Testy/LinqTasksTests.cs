d229ccad7e09ab0c5cb2499b0099e63e4cda81e1
Pawel Borowiec <pawel.2020.borowiec@gmail.com>
2021-04-13 23:26:27 +0200

    Up to 11 example done

---
+++ CW6/CW6_Testy/LinqTasksTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinqTutorials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqTutorials.Models;
+
+namespace LinqTutorials.Tests
+{
+    [TestClass()]
+    public class LinqTasksTests
+    {
+        [TestMethod()]
+        public void Task1Test()
+        {
+            Assert.IsTrue(LinqTasks.Task1().Contains(LinqTasks.Emps.ToList()[0]));
+            Assert.IsTrue(LinqTasks.Task1().Contains(LinqTasks.Emps.ToList()[4]));
+        }
+        [TestMethod()]
+        public void Task2Test()
+        {
+            Assert.IsTrue(LinqTasks.Task2().Contains(LinqTasks.Emps.ToList()[1]));
+            Assert.IsTrue(LinqTasks.Task2().Contains(LinqTasks.Emps.ToList()[2]));
+            Assert.IsTrue(LinqTasks.Task2().Contains(LinqTasks.Emps.ToList()[3]));
+        }
+
+        [TestMethod()]
+        public void Task3Test()
+        {
+            Assert.AreEqual(12000,LinqTasks.Task3());
+        }
+
+        [TestMethod()]
+        public void Task4Test()
+        {
+            Assert.IsTrue(LinqTasks.Task4().Contains(LinqTasks.Emps.ToList()[8]));
+        }
+
+        [TestMethod()]
+        public void Task8Test()
+        {
+            Assert.IsTrue(LinqTasks.Task8());
+        }
+
+
+        [TestMethod()]
+        public void Task9Test()
+        {
+
+            Assert.AreEqual(LinqTasks.Emps.ToList()[1],LinqTasks.Task9());
+        }
+    }
+}
---
+++ CW6/LinqTutorials/Program.cs
@@ -0,0 +1,25 @@
+﻿using LinqTutorials.Models;
+using System;
+using System.Collections.Generic;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*");
+            IEnumerable<Emp> t = LinqTasks.Task1();
+            foreach (Emp x in t)
+            {
+                Console.WriteLine(x.ToString());
+            }
+            IEnumerable<Emp> t2 = LinqTasks.Task2();
+            foreach (Emp x in t2)
+            {
+                Console.WriteLine(x.ToString());
+            }
+
+        }
+    }
+}
