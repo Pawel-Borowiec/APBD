fcb2165c8df9d34dafbbc65393b592333aa6d361
Pawel Borowiec <pawel.2020.borowiec@gmail.com>
2021-04-22 00:34:11 +0200

    cw7 inization

---
+++ CW7/CW7/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using CW7.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW7.Controllers
+{
+    [Route("api/clients")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult getClients()
+        {
+            var context = new s18986Context();
+            return Ok(context.Clients);
+        }
+    }
+}
